d1aacb84ed8e5e603c9db7f40227cf4495734846
Marti Pericay <marti.pericay@gmail.com>
2015-04-16 13:51:20 +0200

    cerdanyola example

--- print/examples/cerdanyola.js
+++ print/examples/cerdanyola.js
@@ -1,7 +1,7 @@
 var json2={
     "map": {
         "title": "Cerdanyola: urbanisme",
-        "size": 1200,
+        "size": 1500,
         "scale": 5000,
         "epsg": 23031,        
         "geographic": false,
@@ -18,17 +18,17 @@ var json2={
         {
             "name": "Mapserver Cerdanyola planejament",
 			"type": "wms",
-            "url": "http://sig.cerdanyola.cat/wms56/cerdanyola/servidor/planejament?FORMAT=image%2Fpng&TRANSPARENT=true&VERSION=1.1.1&SERVICE=WMS&REQUEST=GetMap&STYLES=&LAYERS=qualificacions,cataleg,expedients&SRS=EPSG%3A25831&BBOX=424476.90754292,4592004.2104021,429795.02967114,4594613.0006599&WIDTH=1507&HEIGHT=739",
+            "url": "http://dev.geodata.es/wms56/cerdanyola/servidor/planejament?FORMAT=image%2Fpng&TRANSPARENT=true&VERSION=1.1.1&SERVICE=WMS&REQUEST=GetMap&STYLES=&LAYERS=qualificacions,cataleg,expedients&SRS=EPSG%3A25831&BBOX=424476.90754292,4592004.2104021,429795.02967114,4594613.0006599&WIDTH=1507&HEIGHT=739",
             "opacity": 0.8,
             "layers": [{
                 "title": "Qualificacions",
-                "legend": "http://sig.cerdanyola.cat/wms56/cerdanyola/servidor/planejament?REQUEST=GetLegendGraphic&VERSION=1.1.1&LAYER=qualificacions&FORMAT=image/png&SERVICE=WMS"
+                "legend": "http://dev.geodata.es/wms56/cerdanyola/servidor/planejament?REQUEST=GetLegendGraphic&VERSION=1.1.1&LAYER=qualificacions&FORMAT=image/png&SERVICE=WMS"
             },{
                 "title": "Catàleg",
-                "legend": "http://sig.cerdanyola.cat/wms56/cerdanyola/servidor/planejament?REQUEST=GetLegendGraphic&VERSION=1.1.1&LAYER=cataleg&FORMAT=image/png&SERVICE=WMS"
+                "legend": "http://dev.geodata.es/wms56/cerdanyola/servidor/planejament?REQUEST=GetLegendGraphic&VERSION=1.1.1&LAYER=cataleg&FORMAT=image/png&SERVICE=WMS"
             },{
                 "title": "Expedients",
-                "legend": "http://sig.cerdanyola.cat/wms56/cerdanyola/servidor/planejament?REQUEST=GetLegendGraphic&VERSION=1.1.1&LAYER=expedients&FORMAT=image/png&SERVICE=WMS"
+                "legend": "http://dev.geodata.es/wms56/cerdanyola/servidor/planejament?REQUEST=GetLegendGraphic&VERSION=1.1.1&LAYER=expedients&FORMAT=image/png&SERVICE=WMS"
             }]            
         }],
         "config": {
